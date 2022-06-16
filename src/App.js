c5d8f9e630aeb7b501514aa9d6e1e20c1584437f
Shible Chowdhury <Shible.Chowdhury@theaccessgroup.com>
2022-06-16 22:02:32 +0800

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { ThemeProvider } from '@emotion/react';
+import React from 'react';
+import { createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import Masonry from '@mui/lab/Masonry';
+
+const heights = [150, 30, 90, 70, 110, 150, 130, 80, 50, 90, 100, 150, 30, 50, 80];
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(0.5),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+const theme = createTheme({
+  palette: {
+    background: {
+      default: "#0A1929"
+    },
+    text: {
+      primary: "#b2bac2"
+    }
+  }
+});
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <React.Fragment>
+        <CssBaseline />
+          <Box sm={{ width: 500, minHeight: 393 }}>
+            <Masonry columns={5} spacing={2}>
+              {heights.map((height, index) => (
+                <Item key={index} sx={{ height }}>
+                  {index + 1}
+                </Item>
+              ))}
+            </Masonry>
+          </Box>
+      </React.Fragment>
+    </ThemeProvider>
   );
 }
 
